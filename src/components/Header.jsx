7c15d2fe84655b5cbd82a50a38f108593b97d7b7
Francesco Floris <francescofloris.ef@gmail.com>
2026-04-27 16:55:28 +0200

    fix+ contenuto header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import HeaderNavbarItem from './HeaderNavbarItem.jsx';
 
 function Header() {
     return <header>
+        <a href=""><img src="/imgs/dc-logo.png" alt="" /></a>
         <HeaderNavbarItem />
     </header>;
 }
--- src/components/HeaderNavbarItem.jsx
+++ src/components/HeaderNavbarItem.jsx
@@ -1,5 +1,27 @@
 function HeaderNavbarItem() {
-    return;
+    const items = [
+        { name: "Characters" },
+        { name: "Comics" },
+        { name: "Movies" },
+        { name: "TV" },
+        { name: "Games" },
+        { name: "Collectibles" },
+        { name: "Videos" },
+        { name: "Fans" },
+        { name: "News" },
+        { name: "Shop" }
+    ];
+
+
+
+    return (
+        <>
+            {items.map((item) => (
+                <a href="">{item.name}</a>
+            ))}
+        </>
+    );
+
 }
 
 export default HeaderNavbarItem;
